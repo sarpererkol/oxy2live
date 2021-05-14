fb1e2f42c0c4fb69f000c24ffa87651acae84d46
Sarper ERKOL <sarpererkol@Sarper-MacBook-Pro.local>
2021-05-14 14:15:51 +0300

    initilaze

---
+++ app.js
@@ -0,0 +1,45 @@
+
+var x;
+
+async function func() {
+  const result = await fetch(
+    "http://blynk-cloud.com/E9sQ_1ZWxpBiHE-wPIqjfX5AO5C4JHTY/get/v8"
+  );
+  const data = await result.json();
+  x = data[0];
+
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+    type: "doughnut",
+    data: {
+        labels: ["Oxygen Rate"],
+        datasets: [
+        {
+            label: ["# of Votes"],
+            data: [x, 500 - x],
+            backgroundColor: ["rgba(100, 99, 132, 0.8)"],
+            borderColor: ["rgba(255,99,132,1)"],
+            borderWidth: 1
+        }
+        ]
+    },
+
+    options: {
+        maintainAspectRatio: false,
+        circumference: Math.PI + 1,
+        rotation: -Math.PI - 0.5,
+        cutoutPercentage: 64,
+
+        onClick(...args) {
+        console.log(args);
+        }
+    }
+    });
+}
+
+
+
+func();
+
+
+
