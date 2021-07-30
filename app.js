95a6a5869fcb8730128d99e652850409c636dfa3
sarpererkol <74703597+sarpererkol@users.noreply.github.com>
2021-07-30 16:59:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ async function func() {
     var myChart = new Chart(ctx, {
     type: "doughnut",
     data: {
-        labels: ["Oxygen Sarper"],
+        labels: ["Oxygen Rate"],
         datasets: [
         {
             label: ["# of Votes"],
